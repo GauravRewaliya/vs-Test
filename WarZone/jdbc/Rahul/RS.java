4b2b95ca5ae60d26be6df06ed61c2c40dc17af55
GauravRewaliya <GauravRewaliya@gmail.com>
2022-09-15 09:30:13 +0000

    echo

---
+++ WarZone/jdbc/Rahul/RS.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sample{
+    public static void main(String args[])
+    {
+    System.out.println("RSS");
+    Scanner sc = new Scanner(System.in);//import java.util.*;
+    int age =sc.nextInt();
+    System.out.println("RS age is "+age);
+    }
+}
--- WarZone/jdbc/Rahul/textReader;dd.java
+++ WarZone/jdbc/Rahul/textReader;dd.java
@@ -1,8 +1,32 @@
 import java.io.*;
 class Sample
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws Exception
     {
-        FileInput fin = FileInputStream("TableSchema.txt");
+        
+       // InputStream fin =new FileInputStream("TableSchema.txt");
+
+        FileReader fr = new FileReader("TableSchema.txt"); ///throws exc
+        BufferedReader in = new BufferedReader(fr);
+        String str="",t="";
+        // while((str=in.readLine())!=null) /// print asit is
+        // { 
+        //     System.out.println(str);
+        // }
+        
+
+        // while((t=in.readLine())!=null) /// print asit is
+        //  str+=t;
+        // //System.out.println(str);
+        // String ans[] = str.split(";");
+        // for(String tem : ans)
+        //     System.out.println(tem+";");
+
+        
+
+
+        in.close();
+        fr.close();
+        
     }
 }
